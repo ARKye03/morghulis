d067c63c3facc7e87325c60e2536ed747db5527c
ARKye03 <rafa03-dev@proton.me>
2024-10-13 13:41:10 -0400

    fix: Ambiguity between tags and version

--- scripts/genVersion.sh
+++ scripts/genVersion.sh
@@ -1,5 +1,6 @@
-# Get active tag
-tag=$(git describe --tags --abbrev=0)
+# Generate new version
+tag=$1
 
 echo $tag > version
+git tag -s $tag -m "bump version $tag"
 git cliff --current -o CHANGELOG.md
