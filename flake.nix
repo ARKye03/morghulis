4ccb005335fb3ee1b17f0b21a1a3b627b9b109d7
ARKye03 <rafa03-dev@proton.me>
2024-11-27 13:56:56 -0500

    feat: Refactor flake.nix for improved build process and packaging

--- flake.nix
+++ flake.nix
@@ -9,128 +9,162 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, astal }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          version = builtins.replaceStrings [ "\n" ] [ "" ] (builtins.readFile ./version);
-          buildName = "morghulis";
-          appName = "${buildName}-${version}";
-          cliBuildName = "morghulctl";
-          cliAppName = "morghulctl-${version}";
-          stdenv = pkgs.gcc14Stdenv;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      astal,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        version = builtins.replaceStrings [ "\n" ] [ "" ] (builtins.readFile ./version);
+        buildName = "morghulis";
+        appName = "${buildName}-${version}";
+        cliBuildName = "morghulctl";
+        cliAppName = "morghulctl-${version}";
+        stdenv = pkgs.gcc14Stdenv;
 
-          nix-utils = with pkgs; [
-            nixd
-            nixpkgs-fmt
-          ];
-          nix-morghulis = stdenv.mkDerivation {
-            name = buildName;
-            src = ./.;
-            version = version;
+        nix-utils = with pkgs; [
+          nixd
+          nixfmt-rfc-style
+        ];
+        nix-morghulis = stdenv.mkDerivation {
+          name = buildName;
+          src = ./.;
+          version = version;
 
-            buildInputs = with pkgs;  [
+          buildInputs =
+            with pkgs;
+            [
               pkg-config
-            ] ++ gtk-utils ++ compiler-utils ++ build-utils ++ astal-libs;
+            ]
+            ++ gtk-utils
+            ++ compiler-utils
+            ++ build-utils
+            ++ astal-libs;
 
-            buildPhase = ''
-              mkdir -p $TMPDIR/buildNix
-              cd $TMPDIR/buildNix
-              meson setup $TMPDIR/buildNix $src
-              ninja -C $TMPDIR/buildNix
-            '';
-
-            installPhase = ''
-              mkdir -p $out/bin
-              cp -r $TMPDIR/buildNix/src/${buildName} $out/bin/${appName}
-              cp -r $TMPDIR/buildNix/cli/${cliBuildName} $out/bin/${cliAppName}
-              chmod +x $out/bin/${appName} $out/bin/${cliAppName}
-            '';
-
-            meta = with pkgs.lib; {
-              description = "A Desktop Shell for Wayland";
-              license = licenses.wtfpl;
-              homepage = "https://github.com/ARKye03/morghulis";
-              maintainers = with maintainers; [ ARKye03 ];
-            };
-          };
-          fhs-morghulis = nix-morghulis.overrideAttrs {
-            installPhase = ''
-              mkdir -p $out/bin
-              cp -r $TMPDIR/buildNix/src/${buildName} $out/bin/${appName}
-              cp -r $TMPDIR/buildNix/cli/${cliBuildName} $out/bin/${cliAppName}
-              
-              # Patch the binary
-              ${pkgs.patchelf}/bin/patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $out/bin/${appName}
-              ${pkgs.patchelf}/bin/patchelf --set-rpath /lib:/usr/lib $out/bin/${appName}
-              ${pkgs.patchelf}/bin/patchelf --shrink-rpath $out/bin/${appName}
-              
-              # Patch the cli binary
-              ${pkgs.patchelf}/bin/patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $out/bin/${cliAppName}
-              ${pkgs.patchelf}/bin/patchelf --set-rpath /lib:/usr/lib $out/bin/${cliAppName}
-              ${pkgs.patchelf}/bin/patchelf --shrink-rpath $out/bin/${cliAppName}
-              chmod +x $out/bin/${appName} $out/bin/${cliAppName}
-            '';
-          };
-          pkg-tarball = pkgs.runCommand "morghulis-tarball"
-            {
-              buildInputs = [ pkgs.gnutar pkgs.xz ];
-            } ''
-            mkdir -p $out
-            cp ${fhs-morghulis}/bin/${appName} $out/
-            cp ${fhs-morghulis}/bin/${cliAppName} $out/
-            tar -cJf $out/morghulis-${version}.tar.xz -C $out ${appName} ${cliAppName}
+          buildPhase = ''
+            mkdir -p $TMPDIR/buildNix
+            cd $TMPDIR/buildNix
+            meson setup $TMPDIR/buildNix $src
+            ninja -C $TMPDIR/buildNix
           '';
-          gtk-utils = with pkgs; [
-            gtk4
-            gtk4-layer-shell
-            libadwaita
-          ];
-          compiler-utils = with pkgs; [
-            vala
-            vala-language-server
-            vala-lint
-            uncrustify
-            dart-sass
-            blueprint-compiler
-            git-cliff
-            desktop-file-utils
-          ];
-          build-utils = with pkgs.buildPackages; [
-            muon
-            meson
-            ninja
-          ];
-          astal-libs = with astal.packages.${system}; [
-            hyprland
-            wireplumber
-            mpris
-            network
-            notifd
-            river
-            apps
-            tray
-            io
-            astal4
-            battery
-            powerprofiles
-            bluetooth
-          ];
-          shell = pkgs.mkShell.override
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r $TMPDIR/buildNix/src/${buildName} $out/bin/${appName}
+            cp -r $TMPDIR/buildNix/cli/${cliBuildName} $out/bin/${cliAppName}
+            chmod +x $out/bin/${appName} $out/bin/${cliAppName}
+          '';
+
+          meta = with pkgs.lib; {
+            description = "A Desktop Shell for Wayland";
+            license = licenses.wtfpl;
+            homepage = "https://github.com/ARKye03/morghulis";
+            maintainers = with maintainers; [ ARKye03 ];
+          };
+        };
+        fhs-morghulis = nix-morghulis.overrideAttrs {
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r $TMPDIR/buildNix/src/${buildName} $out/bin/${appName}
+            cp -r $TMPDIR/buildNix/cli/${cliBuildName} $out/bin/${cliAppName}
+
+            # Patch the binary
+            ${pkgs.patchelf}/bin/patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $out/bin/${appName}
+            ${pkgs.patchelf}/bin/patchelf --set-rpath /lib:/usr/lib $out/bin/${appName}
+            ${pkgs.patchelf}/bin/patchelf --shrink-rpath $out/bin/${appName}
+
+            # Patch the cli binary
+            ${pkgs.patchelf}/bin/patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $out/bin/${cliAppName}
+            ${pkgs.patchelf}/bin/patchelf --set-rpath /lib:/usr/lib $out/bin/${cliAppName}
+            ${pkgs.patchelf}/bin/patchelf --shrink-rpath $out/bin/${cliAppName}
+            chmod +x $out/bin/${appName} $out/bin/${cliAppName}
+          '';
+        };
+        pkg-tarball =
+          pkgs.runCommand "morghulis-tarball"
+            {
+              src = ./.;
+              buildInputs = [
+                pkgs.gnutar
+                pkgs.xz
+              ];
+            }
+            ''
+              filenames=(
+                "data/assets/colloid-morghulis-system-hibernate-symbolic.svg"
+                "data/assets/colloid-morghulis-system-lock-screen-symbolic.svg"
+                "data/assets/colloid-morghulis-system-reboot-symbolic.svg"
+                "data/assets/colloid-morghulis-system-shutdown-symbolic.svg"
+                "data/assets/colloid-morghulis-system-suspend-symbolic.svg"
+                "data/desktop/com.github.ARKye03.morghulis.desktop.in"
+                "data/desktop/com.github.ARKye03.morghulis.png"
+              )
+              mkdir -p $out
+              cp ${fhs-morghulis}/bin/${appName} $out/
+              cp ${fhs-morghulis}/bin/${cliAppName} $out/
+              for filename in "''${filenames[@]}"; do
+                cp -r $src/$filename $out/
+              done
+              echo "Taring"
+              tar -cJf $out/morghulis-${version}.tar.xz -C $out .
+            '';
+        gtk-utils = with pkgs; [
+          gtk4
+          gtk4-layer-shell
+          libadwaita
+        ];
+        compiler-utils = with pkgs; [
+          vala
+          vala-language-server
+          vala-lint
+          uncrustify
+          dart-sass
+          blueprint-compiler
+          git-cliff
+          desktop-file-utils
+        ];
+        build-utils = with pkgs.buildPackages; [
+          muon
+          meson
+          ninja
+        ];
+        astal-libs = with astal.packages.${system}; [
+          hyprland
+          wireplumber
+          mpris
+          network
+          notifd
+          river
+          apps
+          tray
+          io
+          astal4
+          battery
+          powerprofiles
+          bluetooth
+        ];
+        shell =
+          pkgs.mkShell.override
             {
               stdenv = stdenv;
             }
             {
-              nativeBuildInputs = with pkgs.buildPackages; [
-                glfw-wayland
-                gobject-introspection
-              ]
-              ++ nix-utils
-              ++ gtk-utils
-              ++ compiler-utils
-              ++ build-utils
-              ++ astal-libs;
+              nativeBuildInputs =
+                with pkgs.buildPackages;
+                [
+                  glfw-wayland
+                  gobject-introspection
+                ]
+                ++ nix-utils
+                ++ gtk-utils
+                ++ compiler-utils
+                ++ build-utils
+                ++ astal-libs;
               buildInputs = with pkgs; [
                 pkg-config
                 networkmanager
@@ -142,24 +176,24 @@
               XCURSOR_THEME = "Bibata-Modern-Classic";
               XCURSOR_SIZE = "20";
             };
-        in
-        {
-          packages = {
-            default = nix-morghulis;
-            fhs = fhs-morghulis;
-            tarball = pkg-tarball;
+      in
+      {
+        packages = {
+          default = nix-morghulis;
+          fhs = fhs-morghulis;
+          tarball = pkg-tarball;
+        };
+        apps = {
+          default = {
+            type = "app";
+            program = "${nix-morghulis}/bin/${appName}";
           };
-          apps = {
-            default = {
-              type = "app";
-              program = "${nix-morghulis}/bin/${appName}";
-            };
-            fhs = {
-              type = "app";
-              program = "${fhs-morghulis}/bin/${appName}";
-            };
+          fhs = {
+            type = "app";
+            program = "${fhs-morghulis}/bin/${appName}";
           };
-          devShells.default = shell;
-        }
-      );
+        };
+        devShells.default = shell;
+      }
+    );
 }
