81ef55b139b7ce7f355ee68938c6cb69995154f3
ARKye03 <rafa03-dev@proton.me>
2025-01-26 01:04:22 -0500

    style: format code for consistency in ast.h

--- lib/Math/ast.h
+++ lib/Math/ast.h
@@ -1,25 +1,25 @@
 #pragma once
 
 typedef enum {
-    NODE_NUMBER,
-    NODE_BINARY_OP
+	NODE_NUMBER,
+	NODE_BINARY_OP
 } NodeType;
 
 typedef enum {
-    OP_ADD,
-    OP_SUBTRACT,
-    OP_MULTIPLY,
-    OP_DIVIDE
+	OP_ADD,
+	OP_SUBTRACT,
+	OP_MULTIPLY,
+	OP_DIVIDE
 } OperatorType;
 
 typedef struct ASTNode {
-    NodeType type;
-    union {
-        double number;
-        struct {
-            OperatorType op;
-            struct ASTNode* left;
-            struct ASTNode* right;
-        } binary;
-    } data;
+	NodeType type;
+	union {
+		double number;
+		struct {
+			OperatorType op;
+			struct ASTNode* left;
+			struct ASTNode* right;
+		} binary;
+	} data;
 } ASTNode;
